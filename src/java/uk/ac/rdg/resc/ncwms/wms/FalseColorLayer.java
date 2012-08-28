ea24e22c8dad9d4b90e9a69d806eca2c5e8bdd44
Ben Caradoc-Davies <Ben.Caradoc-Davies@csiro.au>
2012-08-28 09:27:36 +0800

    More javadocs

--- src/java/uk/ac/rdg/resc/ncwms/wms/FalseColorLayer.java
+++ src/java/uk/ac/rdg/resc/ncwms/wms/FalseColorLayer.java
@@ -8,10 +8,22 @@ package uk.ac.rdg.resc.ncwms.wms;
  */
 public interface FalseColorLayer extends ScalarLayer {
 
+    /**
+     * Get the layer whose values are interpreted as the red component of the
+     * false colour layer.
+     */
     public ScalarLayer getRedComponent();
 
+    /**
+     * Get the layer whose values are interpreted as the green component of the
+     * false colour layer.
+     */
     public ScalarLayer getGreenComponent();
 
+    /**
+     * Get the layer whose values are interpreted as the blue component of the
+     * false colour layer.
+     */
     public ScalarLayer getBlueComponent();
 
 }
